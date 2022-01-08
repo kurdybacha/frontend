958c741784493eb48d45a4c05ed87bfa09d7286c
Kevalin <kevalin@Kevalins-MacBook-Pro.local>
2022-01-08 06:53:19 -0800

    Add metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,5 @@
-import Link from 'next/link'
-import { Fragment, Component } from 'react'
-import Meta from '@/components/Meta'
-import { Popover, Transition } from '@headlessui/react'
-import HyperscaleMark from '@/components/HyperscaleMark'
-import { MenuIcon, XIcon, MapIcon, LightningBoltIcon, UserGroupIcon } from '@heroicons/react/outline'
-
-const navigation = [
-	{ name: 'Discord', href: 'https://discord.com/invite/pVSbzYny2c' },
-	{ name: 'Twitter', href: 'https://twitter.com/HyperscaleFund' },
-]
-
-const airtableUrl = 'https://airtable.com/shrLFCXD7BQXUg97K'
-
 // components
+import Meta from '../components/Meta'
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
@@ -20,39 +7,9 @@ import Section4 from '../components/Section4'
 import Section5 from '../components/Section5'
 import Footer from '../components/Footer'
 
-class ApplyButtonNav extends Component {
-	render() {
-		return (
-			<a
-				href={airtableUrl}
-				className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-			>
-				Apply
-			</a>
-		)
-	}
-}
-
-const features = [
-	{
-		name: 'High speed funding',
-		icon: LightningBoltIcon,
-		desc: 'Fill out an application and get funded in a few days.',
-	},
-	{
-		name: 'Strong community',
-		icon: UserGroupIcon,
-		desc: 'Hyperscale investments are plugged into our community of developers, experts, & fellow founders.',
-	},
-	{
-		name: 'Guidance',
-		icon: MapIcon,
-		desc: 'Our network of advisors are here to support and accelerate your work. We work as one to further each other.',
-	},
-]
-
 const Landing = () => (
 	<div>
+		<Meta />
 		<Section1 />
 		<Section2 />
 		<Section3 />
