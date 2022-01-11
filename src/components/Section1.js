e736d826227ec2e2b426aff071fd148377f4c09d
Ben Harrison <bmhtech07@users.noreply.github.com>
2022-01-11 23:00:02 +0000

    Add padding to stop spillover appearance

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -11,7 +11,7 @@ const links = [
 const Section1 = () => (
 	<div className="bg-no-repeat bg-cover bg-[#160A2F]" style={{ backgroundImage: "url('/section1/1-hero-BG.jpg')" }}>
 		<Header />
-		<div className="flex flex-col-reverse lg:flex-row justify-evenly items-center space-x-3 pt-16 pb-32 lg:py-32 px-8">
+		<div className="flex flex-col-reverse lg:flex-row justify-evenly items-center space-x-3 md:px-20 pt-16 pb-32 lg:py-32">
 			<div className="flex flex-col max-w-3xl mt-12">
 				<h1 className="font-redrose text-gray-100 font-bold text-6xl md:text-8xl">An ecosystem of <span className="hs-gradient">web3</span> projects</h1>
 				<div className="text-white text-2xl my-12">{text}</div>
