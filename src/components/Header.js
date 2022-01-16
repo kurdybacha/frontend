8f8a4bcc5eb698593c1e1e466f3c285fb4d75ac1
Ben Harrison <bmhtech07@users.noreply.github.com>
2022-01-16 00:42:56 +0000

    Set coins at breakpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,8 +13,8 @@ const links = [
 ]
 
 const Header = () => (
-	<nav>
-      	<Popover className="container mx-auto flex items-center justify-between px-8 xl:px-0 pt-4">
+	<nav className="w-full px-10">
+      	<Popover className="container mx-auto flex items-center justify-between py-6">
 			<Link href="/">
 				<a className="flex items-center">
 					<img src="/logo.svg" width="60" />
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -5,10 +5,12 @@ const links = ['https://airtable.com/shrLFCXD7BQXUg97K']
 
 const Section2 = () => (
 	<div className="relative bg-[#1602AS]">
-		<img src="/coins/token.svg" className="absolute w-40 -top-20 left-64" />
-		<img src="/coins/token2.svg" className="absolute w-28 -bottom-20 right-64" />
 		<div className="bg-[#160A2F] md:px-10">
-			<div className="container mx-auto bg-[#FEDB9E] py-24 rounded px-8 xl:px-32">
+			<div className="container relative mx-auto bg-[#FEDB9E] py-24 rounded px-8 xl:px-16">
+				<img src="/coins/token.svg" className="absolute w-40 -top-20 -translate-x-1/2 left-1/2 lg:hidden" />
+				<img src="/coins/token.svg" className="absolute w-40 -top-20 hidden lg:block" />
+				<img src="/coins/token2.svg" className="absolute w-28 -bottom-20 -translate-x-1/2 left-1/2 lg:hidden" />
+				<img src="/coins/token2.svg" className="absolute w-28 -bottom-20 hidden lg:block lg:right-16" />
 				<div className="flex flex-col justify-between items-center">
 					<h2 className="font-redrose text-5xl text-center">Join the Hyperscale Ecosystem</h2>
 					<div className="text-xl mt-4">{text}</div>
