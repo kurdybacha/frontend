9938f56c4b598cba3bf3973827b63d13450c3888
soitsu <97986952+soitsu3@users.noreply.github.com>
2022-01-19 19:22:04 +0000

    Fix div formatting

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -23,12 +23,16 @@ export async function getStaticProps() {
 }
 
 const Privacy = ({ privacyHtml }) => (
-	<div className="bg-[#160A2F] md:p-8 relative">
-		<Header />
-		<div className="container mx-auto pt-8 xl:px-32">
-			<div className="container mx-auto bg-[#EBE8E3] rounded py-24 px-4 sm:px-10 xl:px-32">
-				<h2 className="font-redrose text-5xl pb-12">Privacy Policy</h2>
-				<div className="markdown" dangerouslySetInnerHTML={{ __html: privacyHtml }} />
+	<div>
+		<div className="bg-no-repeat bg-cover bg-[#160A2F] pb-16 px-6 md:px-10">
+			<Header />
+			<div className="bg-[#160A2F] md:p-8 relative">
+				<div className="container mx-auto pt-8 xl:px-32">
+					<div className="container mx-auto bg-[#EBE8E3] rounded py-24 px-4 sm:px-10 xl:px-32">
+						<h2 className="font-redrose text-5xl pb-12">Privacy Policy</h2>
+						<div className="markdown" dangerouslySetInnerHTML={{ __html: privacyHtml }} />
+					</div>
+				</div>
 			</div>
 		</div>
 		<Footer />
