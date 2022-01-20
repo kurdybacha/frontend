bcdcaf6dae3d8ea1f60275b876012d18110f197f
Ben Harrison <bmhtech07@users.noreply.github.com>
2022-01-20 20:18:33 +0000

    Resize hero image and section 1

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -12,12 +12,12 @@ const Section1 = () => (
 	<div className=" bg-[#160A2F] pb-16 lg:pb-32 px-6 md:px-10 bg-curvature">
 		<div className="noise"></div>
 		<Header />
-		<div className="container mx-auto flex flex-col-reverse lg:flex-row justify-between items-center space-x-2 pt-8 md:pt-16 pb-32 lg:pb-24 xl:px-32">
-			<div className="flex flex-col max-w-3xl mt-12">
-				<h1 className="font-redrose text-gray-100 font-bold text-4xl md:text-6xl xl:text-8xl">
+		<div className="container mx-auto flex flex-col-reverse md:flex-row justify-between items-center pt-8 md:pt-16 pb-32 lg:pb-24">
+			<div className="flex flex-col mt-12 lg:max-w-2xl">
+				<h1 className="font-redrose text-gray-100 font-bold text-4xl md:text-5xl xl:text-7xl 2xl:text-8xl">
 					An ecosystem of <span className="hs-gradient">web3</span> projects
 				</h1>
-				<div className="text-white text-lg md:text-2xl my-6 md:my-12">{text}</div>
+				<div className="text-white text- md:text-xl lg: my-6 md:my-12">{text}</div>
 				<Link href={links[2]}>
 					<a target="_blank">
 						<div className="flex w-24 h-10 bg-[#BAC4FA] rounded-sm justify-center items-center font-semibold">
@@ -26,7 +26,7 @@ const Section1 = () => (
 					</a>
 				</Link>
 			</div>
-			<div className="mx-auto max-w-xs px-8 md:min-w-0 lg:max-w-2xl">
+			<div className="mx-auto max-w-sm md:max-w-4xl md:min-w-0">
 				<img src="/section1/hero-graphic.png" />
 			</div>
 		</div>
