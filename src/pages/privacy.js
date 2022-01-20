96a255d076399708fbec1b62f2f9f5f49a289291
soitsu <97986952+soitsu3@users.noreply.github.com>
2022-01-20 10:42:40 +0000

    Remove double hash

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -25,7 +25,7 @@ export async function getStaticProps() {
 const Privacy = ({ privacyHtml }) => (
 	<div>
 		<div
-			className="bg-no-repeat bg-cover bg-[##160A2F] pb-16 px-6 md:px-10"
+			className="bg-no-repeat bg-cover bg-[#160A2F] pb-16 px-6 md:px-10"
 			style={{ backgroundImage: "url('./section1/background.svg')" }}
 		>
 			<Header />
