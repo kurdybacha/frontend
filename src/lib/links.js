316e60b1a7b4f408b8b2adb5e2ef605596ef7dfb
bmhtech07 <64862099+bmhtech07@users.noreply.github.com>
2022-01-31 09:19:49 +0000

    Update links.js
    Add Investment process link

--- src/lib/links.js
+++ src/lib/links.js
@@ -1,5 +1,6 @@
 const links = {
 	knowledgeBase: 'https://hyperscale.notion.site/Investment-Process-17b91837d1aa40c19aec70ba1c79c2b7 ',
+	investmentProcess: 'https://hyperscale.notion.site/Investment-Process-17b91837d1aa40c19aec70ba1c79c2b7',
 	twitter: 'https://www.twitter.com/HyperscaleFund',
 	discord: 'https://discord.gg/7CGPQ8fWHt',
 	apply: 'https://airtable.com/shr0J5cwxZxDU8v7k',
