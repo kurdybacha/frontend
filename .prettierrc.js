6ec2db65a8d65dcdbb2a80718339f3b690de4a53
soitsu <97986952+soitsu3@users.noreply.github.com>
2022-02-21 21:19:38 +0000

    Change prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-	semi: false,
-	singleQuote: true,
-	printWidth: 120,
-	tabWidth: 4,
-	trailingComma: 'es5',
-	useTabs: true,
-	bracketSpacing: true,
-	arrowParens: 'avoid',
+  semi: false,
 }
